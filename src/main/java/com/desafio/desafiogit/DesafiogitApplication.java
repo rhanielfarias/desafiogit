79a4f921dbe8af90fed13bafa4c6667f18a5002d
rhanielfarias <rhanielfarias@gmail.com>
2022-08-31 15:28:54 -0300

    Spring

---
+++ src/main/java/com/desafio/desafiogit/DesafiogitApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.desafiogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiogitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/desafiogit/DesafiogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.desafiogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
