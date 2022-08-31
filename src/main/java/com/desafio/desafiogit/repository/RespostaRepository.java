d0692b6c0fbb4379080b20b77c12ddda13b2eedf
will2375 <wil.liam_benicio@hotmail.com>
2022-08-31 16:13:31 -0300

    Resposta att

---
+++ src/main/java/com/desafio/desafiogit/repository/RespostaRepository.java
@@ -0,0 +1,7 @@
+package com.desafio.desafiogit.repository;
+
+import com.desafio.desafiogit.model.RespostaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RespostaRepository extends JpaRepository<RespostaModel, Long> {
+}
