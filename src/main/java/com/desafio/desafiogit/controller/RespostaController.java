b4038c13ea3437b9a2c67ac30994be8f59121bb0
will2375 <wil.liam_benicio@hotmail.com>
2022-08-31 16:33:20 -0300

    Resposta att

--- src/main/java/com/desafio/desafiogit/controller/RespostaController.java
+++ src/main/java/com/desafio/desafiogit/controller/RespostaController.java
@@ -1,4 +1,23 @@
 package com.desafio.desafiogit.controller;
 
+import com.desafio.desafiogit.model.RespostaModel;
+import com.desafio.desafiogit.service.RespostaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class RespostaController {
-}
+
+    @Autowired
+    RespostaService respostaService;
+
+    @GetMapping(path = "/respostas")
+    public ResponseEntity<List<RespostaModel>> mostrarRespostar(){return ResponseEntity.ok(respostaService.ListDeResposta());}
+
+    @PostMapping(path = "/respostas")
+    public ResponseEntity<RespostaModel>cadastroResposta(@RequestBody RespostaModel respostaModel){
+        return ResponseEntity.ok(respostaService.novaResposta(respostaModel));}
+    }
--- src/main/java/com/desafio/desafiogit/service/RespostaService.java
+++ src/main/java/com/desafio/desafiogit/service/RespostaService.java
@@ -1,4 +1,27 @@
 package com.desafio.desafiogit.service;
 
+import com.desafio.desafiogit.model.RespostaModel;
+import com.desafio.desafiogit.repository.RespostaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RespostaService {
+
+    @Autowired
+    private RespostaRepository respostaRepository;
+
+    public List<RespostaModel>ListDeResposta(){return respostaRepository.findAll();}
+
+
+    public RespostaModel novaResposta(RespostaModel respostaModel){
+        respostaModel.getId();
+        respostaModel.getDataCraicao();
+        respostaModel.getSolucao();
+        respostaModel.getStatus();
+        return respostaRepository.save(respostaModel);
+
+    }
 }
