ba51ae3d23e130f5b11f48c0a228d35d0504cdc4
MaxSouza88 <souza1988@hotmail.com>
2022-09-01 13:25:23 -0300

    Atualizando topico

--- src/main/java/com/desafio/desafiogit/repository/GitRepository.java
+++ src/main/java/com/desafio/desafiogit/repository/GitRepository.java
@@ -1,6 +1,5 @@
 package com.desafio.desafiogit.repository;
 
-import com.desafio.desafiogit.model.RespostaModel;
 import com.desafio.desafiogit.model.TopicoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/desafio/desafiogit/service/TopicoService.java
+++ src/main/java/com/desafio/desafiogit/service/TopicoService.java
@@ -3,7 +3,6 @@ package com.desafio.desafiogit.service;
 import com.desafio.desafiogit.model.TopicoModel;
 import com.desafio.desafiogit.repository.GitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
